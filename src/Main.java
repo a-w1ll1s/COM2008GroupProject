10cc5be2be98b32f6db8c056ecd80593b098140c
opickford <opickford1@sheffield.ac.uk>
2023-12-07 12:46:54 +0000

    Ensuring final commit

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,6 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-
-        /* 
-        ArrayList<Customer> customers = new ArrayList<>();
-        ArrayList<Staff> staffs = new ArrayList<>();
-        ArrayList<Order> pendingOrders = new ArrayList<>();
-
-        try {
-            databaseConnection.openConnection();
-
-            // Fetch data from the database
-            customers.addAll(DatabaseMethods.getCustomerDetails(databaseConnection.getConnection()));
-            staffs.addAll(DatabaseMethods.getStaffDetails(databaseConnection.getConnection()));
-            
-            System.out.println("Fetched orders: " + pendingOrders);
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            databaseConnection.closeConnection();
-        }
-        */
-
         SwingUtilities.invokeLater(() -> new MainFrame("Application"));
     }
 }
